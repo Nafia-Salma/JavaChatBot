8a1a027105357d39c027b87821665d8ca9f6baf7
Nafia-Salma <102663462+Nafia-Salma@users.noreply.github.com>
2022-05-26 16:14:39 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+import tensorflow as tf
+
+
+from chatbot import chat
+
+app=Flask(__name__)
+
+
+@app.get("/")
+def idex_get():
+    return render_template("ChatBot-index.html")
+
+@app.post("/predict")
+def predict():
+    text=request.get_json().get("message")
+    #TODO:check if text is valid
+    response=chat(text)
+    message={"answer":response}
+    return jsonify(message)
+
+
+if __name__==  "__main__":
+        app.run(debug=True)
